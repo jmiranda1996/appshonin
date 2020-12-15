076543879ba66abb244af306edaf5ab54685a290
jerson.miranda <jerson.miranda@outlook.es>
2020-12-15 10:43:16 -0500

    clase 2

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
+# Instalar react native cli
+
 npm install -g react-native-cli
 npx react-native init MyApp
 npm install -D typescript @types/jest @types/react @types/react-native @types/react-test-renderer
 
 https://reactnativeelements.com/docs/
 
+# Instalar axios
+
+npm install axios
+
+#
--- int.source/containers/Login.tsx
+++ int.source/containers/Login.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import { Button, Input, Text } from 'react-native-elements';
+import { ilogin } from '../models/cl_intbas';
 
-class Login extends React.Component {
+interface iprops {
+
+}
+
+class Login extends React.Component<iprops, ilogin> {
+
+    constructor(props: iprops) {
+        super(props)
+        this.state = {
+            ls_usuari: '',
+            ls_passwd: ''
+        }
+    }
+
+    uf_btn_iniciar_sesion = async () => {
+        console.log('a')
+    }
 
     render() {
+
+        const { ls_usuari, ls_passwd } = this.state;
+
         return (
             <View>
                 <Text h1>LOGIN</Text>
+                <Input nativeID={ 'txt_usuari' } placeholder="Usuario" value={ls_usuari} onChangeText={ (as_newval) => this.setState({ls_usuari: as_newval}) }></Input>
+                <Input nativeID={ 'txt_passwd' } placeholder="Password" value={ls_passwd} onChangeText={ (as_newval) => this.setState({ls_passwd: as_newval}) }></Input>
+                <Button onPress={ this.uf_btn_iniciar_sesion }>Iniciar sesion</Button>
             </View>
         )
     }
